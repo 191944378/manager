14a0b607995db767701668693c0841a89d20d5e8
mio <191944378@qq.com>
2020-05-17 08:09:00 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import './assets/css/element-variables.scss'
+import './assets/css/base.css'
+
 
 // 配置 ElementUI
 // import ElementUI from 'element-ui'
