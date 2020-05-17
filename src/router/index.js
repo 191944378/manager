0267e5a764668cb47d39b9c8799468dcc2fc9172
mio <191944378@qq.com>
2020-05-18 03:22:23 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
     { path: '/', redirect: '/login' },
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path == '/login') {
+    if (!localStorage.token) {
+      next()
+    } else {
+      next('/home')
+    }
+  } else {
+    if (!localStorage.token) {
+      next('/login')
+    } else {
+      next()
+    }
+  }
+
+    
+  
 })
 
 export default router
