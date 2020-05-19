19a377f598fb421cbea2547412551d53f6452336
mio <191944378@qq.com>
2020-05-19 08:04:06 +0800

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,52 +1,13 @@
 <template>
-  <el-container class="container">
-    <el-aside width="200px">Aside</el-aside>
-
-    <el-container>
-      <el-header>
-        <el-button icon="el-icon-switch-button" circle @click="loginOut"></el-button>
-      </el-header>
-        
-      <el-main>Main</el-main>
-    </el-container>
-  </el-container>
+  <h2>Home</h2>
 </template>
-
 
 <script>
 export default {
-  methods: {
-    loginOut(){
-      localStorage.removeItem('token')
-      this.$router.push('/login')
-    }
-  },
+  
 }
 </script>
 
-
-<style lang="less" scoped>
-
- .container{
-    height: 100%;
-    background-color: #f8f9fd;
-  }
-
-  .el-header{
-    background-color: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-  }
-  
-  .el-aside {
-    background-color: #fefefe;
-  }
-  
-  .el-main {
-  }
-  
-
-  
+<style lang="stylus" scoped>
 
 </style>
