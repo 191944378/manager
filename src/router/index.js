f8408cc92431a051d5909cd6ec7b5f7abb9601e8
mio <191944378@qq.com>
2020-06-08 21:59:03 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../components/Login.vue'
-import Desktop from '../components/Desktop.vue'
-import Home from '../components/Home.vue'
-import Users from '../components/user/Users.vue'
-import Rights from '../components/power/Rights.vue'
-import Roles from '../components/power/Roles.vue'
-import Cate from '../components/commodity/Cate.vue'
-import Params from '../components/commodity/Params.vue'
-import Goods from '../components/commodity/Goods.vue'
-import Orders from '../components/order/Orders.vue'
-import Reports from '../components/reports/Reports.vue'
+// import Login from '../components/Login.vue'
+// import Desktop from '../components/Desktop.vue'
+// import Home from '../components/Home.vue'
+// import Users from '../components/user/Users.vue'
+// import Rights from '../components/power/Rights.vue'
+// import Roles from '../components/power/Roles.vue'
+// import Cate from '../components/commodity/Cate.vue'
+// import Params from '../components/commodity/Params.vue'
+// import Goods from '../components/commodity/Goods.vue'
+// import Orders from '../components/order/Orders.vue'
+// import Reports from '../components/reports/Reports.vue'
+
+const Login = () => import(/* webpackChunkName: "login-desktop-home" */ '../components/Login.vue')
+const Desktop = () => import(/* webpackChunkName: "login-desktop-home" */ '../components/Desktop.vue')
+const Home = () => import(/* webpackChunkName: "login-desktop-home" */ '../components/Home.vue')
+
+const Users = () => import(/* webpackChunkName: "users" */ '../components/user/Users.vue')
+
+const Rights = () => import(/* webpackChunkName: "rights-roles" */ '../components/power/Rights.vue')
+const Roles = () => import(/* webpackChunkName: "rights-roles" */ '../components/power/Roles.vue')
+
+const Cate = () => import(/* webpackChunkName: "cate-params-goods" */ '../components/commodity/Cate.vue')
+const Params = () => import(/* webpackChunkName: "cate-params-goods" */ '../components/commodity/Params.vue')
+const Goods = () => import(/* webpackChunkName: "cate-params-goods" */ '../components/commodity/Goods.vue')
+
+const Orders = () => import(/* webpackChunkName: "orders" */ '../components/order/Orders.vue')
+
+const Reports = () => import(/* webpackChunkName: "reports" */ '../components/reports/Reports.vue')
+
+
+
 
 Vue.use(VueRouter)
 
