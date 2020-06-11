4ef022c38cf9dc6c2a29aa4c3daa73edb98f4edf
mio <191944378@qq.com>
2020-06-11 23:38:21 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    currentMenu: null,
   },
   mutations: {
+    selectMenu(state, val){
+      val.name == 'home'? state.currentMenu = null : state.currentMenu = val
+    }
   },
   actions: {
   },
